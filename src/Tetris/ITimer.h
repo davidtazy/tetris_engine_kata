6a1d7f75b575d6798c64dfc238b8c062a3455caf
davidtazy <david.allemant@gmail.com>
2020-05-30 19:46:37 +0200

    Refactor , iTimer in its own file

---
+++ src/Tetris/ITimer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+struct ITimer;
+struct TimerListener {
+  virtual void OnTimerEvent(const ITimer& timer) = 0;
+};
+
+struct ITimer {
+  virtual void Start(const std::chrono::milliseconds& period) = 0;
+  virtual void Stop() = 0;
+  bool IsStarted() const { return started; }
+  void Register(TimerListener* listener_p) { listener = listener_p; };
+
+ protected:
+  void Step() {
+    if (listener)
+      listener->OnTimerEvent(*this);
+  }
+
+  bool started{false};
+
+ private:
+  TimerListener* listener{};
+};
--- src/Tetris/Tetris.h
+++ src/Tetris/Tetris.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <chrono>
 #include "Tetris/IScore.h"
+#include "Tetris/ITimer.h"
 #include "Tetris/Tetriminos.h"
 namespace tetris {
 
@@ -20,29 +21,6 @@ struct InputListener {
   virtual void OnFastDown() = 0;
   virtual void OnPause() = 0;
   virtual void OnResume() = 0;
-};
-
-struct ITimer;
-struct TimerListener {
-  virtual void OnTimerEvent(const ITimer& timer) = 0;
-};
-
-struct ITimer {
-  virtual void Start(const std::chrono::milliseconds& period) = 0;
-  virtual void Stop() = 0;
-  bool IsStarted() const { return started; }
-  void Register(TimerListener* listener_p) { listener = listener_p; };
-
- protected:
-  void Step() {
-    if (listener)
-      listener->OnTimerEvent(*this);
-  }
-
-  bool started{false};
-
- private:
-  TimerListener* listener{};
 };
 
 struct Block {
